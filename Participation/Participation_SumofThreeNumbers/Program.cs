16a0d70542f1fec0854a64a753c7fdf4dbf0c140
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-09-01 11:52:22 -0500

    created a new repository

---
+++ Participation/Participation_SumofThreeNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Participation_SumofThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
