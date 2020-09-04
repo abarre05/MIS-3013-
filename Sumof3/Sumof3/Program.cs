f43a96a4f567245074fca721cae5a33890e46203
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-09-03 20:48:40 -0500

    worked on participation Sumof3

---
+++ Sumof3/Sumof3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sumof3
+{
+    class Program
+    {
+        const double INT = "7.777";
+        static void Main(string[] args)
+        {
+Console.WriteLine("Please enter three numbers >>");
+            string NumInput = Console.ReadLine();
+            double Num = Convert.ToDouble(NumInput);
+            Console.WriteLine(Num.ToString);
+        }
+    }
+}
