7bc9c811ffa2a816437419ffe41867f111ef6212
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-09-09 23:49:53 -0500

    started Conditional_CoinToss participation

---
+++ CoinToss/Conditional_CoinToss/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Conditional_CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random rnd = new Random();
+            int number = rnd.Next(1, 3);
+
+        }
+    }
+}
