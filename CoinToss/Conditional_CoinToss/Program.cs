8668a7292dfec6fd91cb3ab732abc4b2b1cf3070
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-09-10 12:17:35 -0500

    worked on ConditionalCoinToss

--- CoinToss/Conditional_CoinToss/Program.cs
+++ CoinToss/Conditional_CoinToss/Program.cs
@@ -6,9 +6,26 @@ namespace Conditional_CoinToss
     {
         static void Main(string[] args)
         {
-            
+
             Random rnd = new Random();
             int number = rnd.Next(1, 3);
+            Console.WriteLine("Please pick heads or tails");
+            string CoinToss = Convert.ToString(Console.ReadLine());
+
+            
+            if (number == 1)
+            {
+                Console.WriteLine("heads");
+            }
+            else
+            {
+                Console.WriteLine("tails");
+            }
+            
+            
+       
+            
+
 
         }
     }
