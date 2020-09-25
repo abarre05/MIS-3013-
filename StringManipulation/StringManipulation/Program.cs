d3bb2f50581744bc24c780bf769bd579c35b0f82
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-09-24 20:12:57 -0500

    started participation for StringManipulation

---
+++ StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        { string sentence = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning";
+            Console.WriteLine("Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.");
+            sentence.Split(' ');
+
+            
+
+            Console.WriteLine("Please pick a word to look for in the sentence above");
+            Console.WriteLine("Please pick a work to change the word to");
+         
+        
+        }
+    }
+}
