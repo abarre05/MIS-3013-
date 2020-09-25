5c8d6295c481bd5eafa806a04d191d4fbe651e89
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-09-25 15:38:44 -0500

    participation StringManipulation

--- StringManipulation/StringManipulation/Program.cs
+++ StringManipulation/StringManipulation/Program.cs
@@ -10,16 +10,25 @@ namespace StringManipulation
     {
         static void Main(string[] args)
 
-        { string sentence = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning";
+        {
+            string sentence = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning";
             Console.WriteLine("Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.");
-            sentence.Split(' ');
+           
+            string word = "today";
 
-            
 
             Console.WriteLine("Please pick a word to look for in the sentence above");
             Console.WriteLine("Please pick a work to change the word to");
-         
-        
+            if (word == "today")
+            {
+            }
+            else if (word != "today")
+            {
+            }
+            else
+                Environment.Exit();
         }
-    }
-}
+
+            }
+        }
+    
