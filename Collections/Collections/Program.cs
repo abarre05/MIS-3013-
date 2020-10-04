30b13b8f51cf9329c85be31943e86fcccc1b6c65
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-10-04 15:23:17 -0500

    Collections participation

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<double> ExamGrades = new List<double>();
+            string answer;
+            
+
+            do
+            {
+                Console.WriteLine("Please enter one of your exam grades");
+                double ExamGrade = Convert.ToDouble(Console.ReadLine());
+
+                ExamGrades.Add(ExamGrade);
+
+                Console.WriteLine("Do you have any other exam grades to tell me?");
+                answer = Console.ReadLine();
+            } while (answer.ToLower() == "yes");
+
+            do
+            {
+                double number;
+                double ExamGrade;
+               if (number > ExamGrade)
+                {
+                    max= number
+                }
+
+            }while ()
+            
+
+
+        }
+    }
+}
