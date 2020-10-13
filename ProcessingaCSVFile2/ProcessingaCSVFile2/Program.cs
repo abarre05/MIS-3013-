f37f232997f2cf2f59f8f523f2d14400e6839f7c
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-10-13 12:08:49 -0500

    started participated ProcessingaCSVFile2

---
+++ ProcessingaCSVFile2/ProcessingaCSVFile2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+namespace ProcessingaCSVFile2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name = 4
+            //Transaction_Date = 0
+            //Payment_Type = 3
+            //Country = 7
+            string filePath = @"C:\Users\abarre05\Downloads\SalesJan2009.csv";
+            string [] lines = File.ReadAllLines(filePath)
+
+
+        }
+    }
+}
