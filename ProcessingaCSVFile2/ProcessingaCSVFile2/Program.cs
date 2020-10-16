5cfb264a25c0ac71ea2250d8e8e18289ee4e2216
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-10-15 19:23:07 -0500

    participation ProcessingaCSVFile2

--- ProcessingaCSVFile2/ProcessingaCSVFile2/Program.cs
+++ ProcessingaCSVFile2/ProcessingaCSVFile2/Program.cs
@@ -11,8 +11,19 @@ namespace ProcessingaCSVFile2
             //Payment_Type = 3
             //Country = 7
             string filePath = @"C:\Users\abarre05\Downloads\SalesJan2009.csv";
-            string [] lines = File.ReadAllLines(filePath)
+            string[] lines = File.ReadAllLines(filePath);
+            string Payment_Type;
+            string Country;
+            string Name;
+            string Transaction_Date;
+            if (Payment_Type = "Visa");
+               (Country = "United States");
 
+            {
+                Console.WriteLine("The name for the transaction is " + Name + " and the Transaction Date is " + Transaction_Date);
+            }
+
+            Console.ReadKey();
 
         }
     }
