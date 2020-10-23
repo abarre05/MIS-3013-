5ad51ba0df5994acd68c87f02614ab6170a5fe76
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-10-23 18:11:38 -0500

    methods participation

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args);
+        {
+           Console.WriteLine("Please enter a type of animal");
+        }
+
+    static void Speak;
+    {
+        
+        
+    }
+}
+
+    
+    
+   
+ 
+
+
