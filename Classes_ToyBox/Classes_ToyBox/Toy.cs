32a7d0cffe6e1c3e6c47ce0e4383620b00937121
Allie Barrett <allison.m.barrett-1@ou.edu>
2020-11-17 12:06:09 -0600

    ToyBox participation

---
+++ Classes_ToyBox/Classes_ToyBox/Toy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_ToyBox
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double price { get; set; }
+        private string Notes() {  }
+        public Toy()
+
+        {
+            Manufacturer = " ";
+            Name = " ";
+
+
+
+        }
+    }
+}
---
+++ Classes_ToyBox/Classes_ToyBox/ToyBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_ToyBox
+{
+    class ToyBox
+    {
+     public List<Toy> Toys { get; set; }
+     public  ToyBox()
+        {
+            Toys = new List<Toy>();
+        }
+
+
+        public GetRandomToy()
+        {
+            var random = new Random();
+           
+
+
+        }
+    }
+}
